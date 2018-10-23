d6e11cd42592755bfceaee80830f571d91c2fe58
Aldo <alduxx@gmail.com>
2018-10-23 19:31:46 -0300

    Static files

---
+++ SolarHeating/static/js/script.js
@@ -0,0 +1,18 @@
+(function () {
+   var g = new JustGage({
+    id: "gauge",
+    value: 28,
+    min: 0,
+    max: 100,
+    title: "Temp Paineis",
+  });
+
+  var g = new JustGage({
+    id: "gauge2",
+    value: 80,
+    min: 0,
+    max: 100,
+    title: "Temp Boiler",
+  });
+
+})();
